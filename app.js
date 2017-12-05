e371c416ab87ef918e141fc3a5ed928d5321e7a4
richardjohns <you@example.com>
2017-12-05 17:05:31 +1300

    added current weather intent

--- app.js
+++ app.js
@@ -14,18 +14,22 @@ rl.setPrompt('> ')
 rl.prompt()
 rl.on('line', reply => {
     matcher(reply, data => {
-        switch(data.intent) {
-            case 'Hello':
-                console.log("A big hello from Vanilla!")
-                rl.prompt()
-                break
-            case 'Exit':
-                console.log("Bye and have a great day!")
-                process.exit(0)
-            default: {
-                console.log("I don't understand")
-                rl.prompt()
-            }
+        switch (data.intent) {
+          case "Hello":
+            console.log(`${data.entities.greeting} to you too`);
+            rl.prompt();
+            break;
+          case "CurrentWeather":
+            console.log(`Checking weather for ${data.entities.city}`);
+            rl.prompt();
+            break;
+          case "Exit":
+            console.log("Bye and have a great day!");
+            process.exit(0);
+          default: {
+            console.log("I don't understand");
+            rl.prompt();
+          }
         }
     })
 })
--- patterns/index.js
+++ patterns/index.js
@@ -1,13 +1,16 @@
 const patternDict = [
-    {
-    pattern: '\\b(Hi|Hello|Hey)\\b',
-    intent: 'Hello'
-    },
-    {
-    pattern: '\\b(bye|exit)\\b',
-    intent: 'Exit'
-    }
-
-]
+  {
+    pattern: "\\b(?<greeting>Hi|Hello|Hey)\\b",
+    intent: "Hello"
+  },
+  {
+    pattern: "\\b(?<exit>bye|exit)\\b",
+    intent: "Exit"
+  },
+  {
+    pattern: 'like\\sin\\s\\b(?<city>.+)',
+    intent: "CurrentWeather"
+  }
+];
 
 module.exports = patternDict
