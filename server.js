495ad9320a789b5f611b6d33375f60513f0a2d13
richardjohns <richard.johns193@gmail.com>
2017-12-07 00:12:40 +1300

    add restify server, add fb token support for API

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict'
+// create an API server
+
+const Restify = require('restify')
+const server = Restify.createServer({
+    name: 'VanillaMessenger'
+})
+const PORT = process.env.PORT || 3000
+
+// when deployed, deployment platform will provide port.
+
+// Tokens
+const config = require('./config')
+
+// Test
+server.get('/', (req, res, next) => {
+    res.send("Hello")
+    return next()
+})
+
+server.listen(PORT, () => console.log(`Vanilla server up!.. on port ${PORT}`))
