a5d2706db2dcd59468fe7efb959e84fbbbcf9cb9
richardjohns <richard.johns193@gmail.com>
2017-12-06 21:47:40 +1300

    bring in weather forecast code

--- parser/index.js
+++ parser/index.js
@@ -1,6 +1,7 @@
 'use strict'
 const dictionary = require('./dictionary')
 const colors = require('colors')
+const moment = require('moment')
 let getFeel = temp => {
     if (temp < 5) {
         return "shivering cold"
@@ -26,15 +27,51 @@ let getPrefix = (conditionCode, tense = 'present') => {
     return findPrefix.prefix || ""
 }
 
+let getDate = day => {
+    let dayStr = day.toLowerCase().trim()
+    switch (dayStr) {
+      case "tomorrow":
+        return moment()
+          .add(1, "d")
+          .format("DD MMM YYYY");
+      case "day after tomorrow":
+        return moment()
+          .add(2, "d")
+          .format("DD MMM YYYY");
+      default:
+        return moment().format("DD MMM YYYY")
+    }
+}
+
 let currentWeather = response => {
     if(response.query.results) {
         let resp = response.query.results.channel
         let location = `${resp.location.city}, ${resp.location.country}`
         let {text, temp, code} = resp.item.condition
         return `Right now, ${getPrefix(code)} ${text.toLowerCase()} in ${location}. It is ${getFeel(Number(temp))} at ${temp.red.bold} degrees Celcius`
+    } else {
+        return "I don't know anything about this place... Sorry :("
+    }
+}
+
+let forecastWeather = response => {
+    if(response.query.results) {
+        let parseDate = getDate(data.time)
+        let resp = response.query.results.channel
+        let getForecast = resp.item.forecast.filter(item => {
+            return item.date === parseDate
+        })[0]
+        // note this addition of [0]
+        let location = `${resp.location.city}, ${resp.location.country}`;
+        let regEx = new RegExp(data.weather, 'i')
+        let testConditions = regEx.test(getForecast.text)
+        return `${testConditions ? 'Yes' : 'No'}, ${getPrefix(getForecast.code, 'future')} ${getForecast.text.bold} ${data.time} in ${location}`
+    } else {
+        return "I don't know anything about this place... Sorry :("
     }
 }
 
 module.exports = {
-    currentWeather
+    currentWeather,
+    forecastWeather
 }
