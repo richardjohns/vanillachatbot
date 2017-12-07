3f9320630ee4a382dc9879bea22ef63187f775ed
richardjohns <richard.johns193@gmail.com>
2017-12-07 17:41:18 +1300

    got webhooks set up - refactor to register webhook object as different

--- config/index.js
+++ config/index.js
@@ -1,10 +1,13 @@
-'use strict'
+"use strict";
 
-if(process.env.NODE_ENV === 'production') {
-    module.exports = {
-        PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN,
-        VERIFY_TOKEN: process.env.VERIFY_TOKEN
-    }
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN,
+    VERIFY_TOKEN: process.env.VERIFY_TOKEN
+  };
 } else {
-    module.exports = require('./development.json')
+  module.exports = require("./development.json");
 }
+
+
+// "VERIFY_TOKEN": "c87b9d1ccf9ffc934a14cf627d2dee16469ba173334339fc"
--- server.js
+++ server.js
@@ -1,25 +1,92 @@
 'use strict'
-// create an API server
+// create an API server - Restify is basically a stripped down version of Express with only API functionality - can use Express instead if desired.
+// in development use ngrok.io utility to provide a secure url eg http or https for the app to receive messages. eg https://something.ngrok.io
+// serve with command ./ngrok http 3000
 
-const Restify = require('restify')
+const Restify = require("restify");
 const server = Restify.createServer({
-    name: 'VanillaMessenger'
-})
-const PORT = process.env.PORT || 3000
-
-// when deployed, deployment platform will provide port.
+  name: "VanillaMessenger"
+});
+const PORT = process.env.PORT || 3000;
 
 // Tokens
-const config = require('./config')
+const config = require("./config");
 
-//FBeamer
-const FBeamer = require('./fbeamer')
-const f = new FBeamer(config)
+// FBeamer
+const FBeamer = require("./fbeamer");
+const f = new FBeamer(config);
 
-// Test
-server.get('/', (req, res, next) => {
-    res.send("Hello")
-    return next()
-})
+// Vanilla
+const matcher = require("./matcher");
+const weather = require("./weather");
+const { currentWeather, forecastWeather } = require("./parser");
 
-server.listen(PORT, () => console.log(`Vanilla server up!.. on port ${PORT}`))
+// server.use(Restify.plugins.acceptParser(server.acceptable));
+server.use(Restify.plugins.jsonp());
+server.use(Restify.plugins.bodyParser());
+
+// Register the webhooks
+server.get("/", (req, res, next) => {
+  f.registerHook(req, res);
+  return next();
+});
+
+// Receive all incoming messages
+server.post("/", (req, res, next) => {
+  f.incoming(req, res, msg => {
+    // Process messages
+    // f.txt(msg.sender, `Hey, you just said ${msg.message.text}`);
+    // f.img(msg.sender, "http://www.stickees.com/files/food/sweet/3543-icecream-cone-sticker.png");
+    if (msg.message.text) {
+      // If a text message is received
+      matcher(msg.message.text, data => {
+        switch (data.intent) {
+          case "Hello":
+            f.txt(msg.sender, `${data.entities.greeting} to you too!`);
+            break;
+          case "CurrentWeather":
+            weather(data.entities.city, "current")
+              .then(response => {
+                let parseResult = currentWeather(response);
+                f.txt(msg.sender, parseResult);
+              })
+              .catch(error => {
+                console.log(
+                  "There seems to be a problem connecting to the weather service"
+                );
+                f.txt(
+                  msg.sender,
+                  "Hmm, something's not right with my servers! Do check back in a while...Sorry :("
+                );
+              });
+            break;
+          case "WeatherForecast":
+            weather(data.entities.city)
+              .then(response => {
+                let parseResult = forecastWeather(response, data.entities);
+                f.txt(msg.sender, parseResult);
+              })
+              .catch(error => {
+                console.log(
+                  "There seems to be a problem connecting to the weather service"
+                );
+                f.txt(
+                  msg.sender,
+                  "Hmm, something's not right with my servers! Do check back in a while...Sorry :("
+                );
+              });
+            break;
+          default: {
+            f.txt(msg.sender, "Gosh! I don't know what you mean :(");
+          }
+        }
+      });
+    }
+  });
+  return next();
+});
+
+// Subscribe
+f.subscribe();
+
+server.listen(PORT, () => console.log(`Vanilla running on port ${PORT}`));
